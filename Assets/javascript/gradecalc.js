7d12347d0259f5e38005a55a97334e40338182d6
Molly Kizer <61219066+LivesInRoom29@users.noreply.github.com>
2021-01-14 15:50:25 -0500

    Add styling to grade result; add letter grade calculation and add to page.

--- Assets/javascript/gradecalc.js
+++ Assets/javascript/gradecalc.js
@@ -5,9 +5,61 @@ $(document).ready(function(){
   const sem2Input = $('input#sem2');
   const examInput = $('input#exam');
   const submitBtn = $('button.get-grade');
-  const clearBtn = $('button.clear');
   const resultEl = $('p.result');
+  const letterGradeEl = $('p.letter-grade');
   const noexamCheckEl = $('input#noexam');
+
+  function getLetterGrade(percentage) {
+    const roundedGrade = Math.round(percentage);
+    let letterGrade = "";
+
+    switch(true) {
+      case percentage >= 99:
+        letterGrade = "A+";
+        break;
+      case roundedGrade <= 98 && roundedGrade >= 96:
+        letterGrade = "A";
+        break;
+      case roundedGrade <= 95 && roundedGrade >= 93:
+        letterGrade = "A-";
+        break;
+      case roundedGrade <= 92 && roundedGrade >= 91:
+        letterGrade = "B+";
+        break;
+      case roundedGrade <= 90 && roundedGrade >= 88:
+        letterGrade = "B";
+        break;
+      case roundedGrade <= 87 && roundedGrade >= 85:
+        letterGrade = "B-";
+        break;
+      case roundedGrade <= 84 && roundedGrade >= 83:
+        letterGrade = "C+";
+        break;
+      case roundedGrade <= 82 && roundedGrade >= 80:
+        letterGrade = "C";
+        break;
+      case roundedGrade <= 79 && roundedGrade >= 77:
+        letterGrade = "C-";
+        break;
+      case roundedGrade <= 76 && roundedGrade >= 75:
+        letterGrade = "D+";
+        break;
+      case roundedGrade <= 74 && roundedGrade >= 73:
+        letterGrade = "D";
+        break;
+      case roundedGrade <= 72 && roundedGrade >= 70:
+        letterGrade = "D-";
+        break;
+      case roundedGrade < 70:
+        letterGrade = "F";
+        break;
+      default:
+        letterGrade = "default";
+    }
+    console.log("rounded grade: ", roundedGrade);
+    console.log("letter grade: ", letterGrade);
+    return letterGrade;
+  }
 
   function calculateGrade() {
     const sem1Grade = parseFloat(sem1Input.val());
@@ -21,12 +73,12 @@ $(document).ready(function(){
     } else {
       semesterGrade = ((sem1Grade + sem2Grade) * 0.4 + examGrade * 0.2).toFixed(2)
     }
-    console.log("sem grade = ", semesterGrade);
-    resultEl.html(`${semesterGrade}`);
+    // Turn the % grade into a letter grade
+    const letterGrade = getLetterGrade(semesterGrade)
+    // Add the semester grade (% and letter grade) to the DOM
+    resultEl.html(`${semesterGrade}%`);
+    letterGradeEl.html(`Letter Grade: ${letterGrade}`);
   };
-
-  function clearForm() {
-  }
 
   submitBtn.on("click", function() {
     calculateGrade();
