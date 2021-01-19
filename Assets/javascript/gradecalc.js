40e5e535df7e17aa4af7adbb86fba753a5f4f5ab
Molly Kizer <61219066+LivesInRoom29@users.noreply.github.com>
2021-01-19 15:43:50 -0500

    Add form validation; format gradecalc.html

--- Assets/javascript/gradecalc.js
+++ Assets/javascript/gradecalc.js
@@ -4,11 +4,35 @@ $(document).ready(function(){
   const quarter1Input = $('input#q1');
   const quarter2Input = $('input#q2');
   const examInput = $('input#exam');
-  const submitBtn = $('button.get-grade');
+  const submitBtn = $('button#get-grade-btn');
   const resultEl = $('p.result');
   const letterGradeEl = $('p.letter-grade');
   const noexamCheckEl = $('input#noexam');
   const graphContainerEl = $('div#graph-container');
+
+  function validateForm(q1, q2, exam) {
+    // return false if:
+    // there is an exam grade if "No Exam" was not cheked
+    if (!exam && !noexamCheckEl.is(":checked")) {
+      alert('You must include an exam grade or check off "Check if no exam" box.');
+      return false;
+    // the exam grade exists, but it's not between 0 and 100
+    } else if (exam && (exam < 0 || exam > 100)) {
+      alert('Your exam grade needs to be a number between 0 and 100.');
+      return false;
+    // q1 and q2 grades are missing
+    } else if (!q1 || !q2) {
+      alert('You need to input a number grade between 0 and 100 for both quarters.');
+      return false;
+    // if q1 and q2 grades are not between 0 and 100
+    } else if (q1 > 100 || q2 > 100 || q1 < 0 || q2 < 0) {
+      alert('Your grades all need to numbers be between 0 and 100.');
+      return false;
+    // otherwise, return true.
+    } else {
+      return true;
+    }
+  };
 
   // To turn the percentage grade into a letter grade using the SHS handbook
   function getLetterGrade(percentage) {
@@ -60,7 +84,7 @@ $(document).ready(function(){
         letterGrade = "default";
     }
     return letterGrade;
-  }
+  };
 
   function calculateGrade() {
     const q1Grade = parseFloat(quarter1Input.val());
@@ -70,25 +94,38 @@ $(document).ready(function(){
     const noexam = noexamCheckEl.is(":checked");
     let semesterGrade;
 
-    // To calculate the semester grade:
-    // if the box is checked, get the average of the two quarter grades
-    // otherwise combine 40% of each semester grade with 20% of the exam grad
-    if (noexam) {
-      semesterGrade = ((q1Grade + q2GRade) / 2).toFixed(2)
-    } else {
-      semesterGrade = ((q1Grade + q2GRade) * 0.4 + examGrade * 0.2).toFixed(2)
+    const isFormValid = validateForm(q1Grade, q2GRade, examGrade);
+    console.log("is form valid? ", isFormValid);
+
+    //only if the form inputs are valid, calculate and display the grade
+    if (isFormValid) {
+      // To calculate the semester grade:
+      // if the box is checked, get the average of the two quarter grades
+      // otherwise combine 40% of each semester grade with 20% of the exam grad
+      if (noexam) {
+        semesterGrade = ((q1Grade + q2GRade) / 2).toFixed(2)
+      } else {
+        semesterGrade = ((q1Grade + q2GRade) * 0.4 + examGrade * 0.2).toFixed(2)
+      }
+      // Turn the % grade into a letter grade
+      const letterGrade = getLetterGrade(semesterGrade)
+      // Add the semester grade (% and letter grade) to the DOM
+      resultEl.html(`${semesterGrade}%`);
+      letterGradeEl.html(`${letterGrade}`);
     }
-    // Turn the % grade into a letter grade
-    const letterGrade = getLetterGrade(semesterGrade)
-    // Add the semester grade (% and letter grade) to the DOM
-    resultEl.html(`${semesterGrade}%`);
-    letterGradeEl.html(`${letterGrade}`);
   };
+  
+  // on("submit") and .submit() are still reloading even with event.preventDefault()??
+  // The tool tips for the vaildation in html (required) only work using "Submit", not "click"
+  // submitBtn.on("submit", function(event) {
+  //   event.preventDefault();
+  //   //event.isPreventDefault(true);
+  //   calculateGrade();
+  //   return false;
+  // });
 
   submitBtn.on("click", function(event) {
     event.preventDefault();
     calculateGrade();
   });
-
-  
 });
